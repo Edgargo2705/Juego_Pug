4bde1e9ce787d83a27ec02e9b002fdf81d9bee96
Edgar <211111@ids.upchiapas.edu,.mx>
2023-01-29 18:10:21 -0600

    commit 3 clase-comida

---
+++ src/main/java/com/example/juego_pug/models/Comida.java
@@ -0,0 +1,43 @@
+package com.example.juego_pug.models;
+
+import java.util.Observable;
+import java.util.Random;
+
+public class Comida extends Observable implements Runnable{
+    private Vector vector;
+    private Random random;
+    private boolean estado;
+
+    public Comida (){
+        estado = true;
+        random = new Random(System.currentTimeMillis());
+    }
+
+    public void set_position(Vector vector){
+        this.vector=vector;
+        vector.setX(random.nextInt(622));
+        vector.setY(random.nextInt(318));
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public void run() {
+        while (estado){
+            setChanged();
+            notifyObservers(vector);
+            try {
+                Thread.sleep(100);
+            }catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
